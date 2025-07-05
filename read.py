b830a7f52e303509b71a44d44372699e9b8f695d
Rayz Chang <dio19880605@gmail.com>
2025-07-06 01:11:46 +0700

    Add Comments

--- read.py
+++ read.py
@@ -18,4 +18,12 @@ new = []
 for d in data:
 	if len(d) < 100:
 		new.append(d)
+#17-20行 快寫法可寫 
+#new = [d for d in data if len(d) < 100]
+#解析: 
+#[d(運算) for(迴圈) d(暫時變數) in(在) data(清單) if(判斷式) len(d) < 100 (篩選條件)]
+#"運算"及"判斷式"可根據需求改變, 也可無, 
+#舉例: 亦可寫 new = [d for d in data] 
+#就是只把清單內的暫時變數增加到new這個新的清單內
+
 print('小於100字的留言為', len(new), '筆資料')
